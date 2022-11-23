28fd019e307ad3874f9b67de68d9494f85be08b1
AlexanderErofeev <109588880+AlexanderErofeev@users.noreply.github.com>
2022-11-23 21:53:47 +0500

    Исправлены опечатки и забытые изменения

--- crop_csv_professions.py
+++ crop_csv_professions.py
@@ -7,13 +7,13 @@ file_list = [path + f for f in os.listdir(path) if f.endswith('.csv')]
 csv_list = []
 
 # keep_col = ['name', 'description', 'key_skills', 'experience_id', 'premium', 'employer_name', 'salary_from', 'salary_to', 'salary_gross', 'salary_currency', 'area_name', 'published_at']
-keep_col = ['name']
+keep_col = ['name', 'salary_from', 'salary_to', 'salary_currency', 'published_at']
 
 for file in sorted(file_list):
     print(os.path.basename(file))
     csv_list.append(pd.read_csv(file, usecols=keep_col))
 
-csv_merged = pd.concat(csv_list, ignore_index=True)
+csv_merged = pd.concat(csv_list, ignore_index=True).reindex(columns=keep_col)
 print(len(csv_merged.index))
 
-csv_merged.to_csv("professions_names.csv", index=False)
+csv_merged.to_csv("vacancies.csv", index=False)
